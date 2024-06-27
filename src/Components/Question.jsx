5e2d754409c8b2a08774df635baac30d1dc467a4
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-06-28 01:56:36 +0530

    Update Question.jsx

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -9,26 +9,26 @@ export default function Question({details}) {
       <QuestionNav />
       <div className={classes.wrapper}>
         <span className={classes.title}>
-          <h1>{details.title}</h1>
-          {details.status && <h2>Solved</h2> }
+          <h1>{details?.title}</h1>
+          {details?.status && <h2>Solved</h2> }
         </span>
         <ul className={classes.tags}>
           {details?.tags?.map((topic) => <li key={topic}>{topic}</li>)}
         </ul>
-        <p className={classes.desc}>{details.description}</p>
+        <p className={classes.desc}>{details?.description}</p>
         <h3>Sample Testcases:</h3>
         <div className={classes.testcases}>
           <div className={classes.test}>
             <p>Input:</p>
             <ul className={classes.values}>
-              <li key="0">{details.testCase?.length}</li>
-              {details.testCase?.map((test, index) => <li key={index+1}><p>{test}</p></li>)}
+              <li key="0">{details?.testCase?.length}</li>
+              {details?.testCase?.map((test, index) => <li key={index+1}><p>{test}</p></li>)}
             </ul>
           </div>
           <div className={classes.test}>
             <p>Output:</p>
             <ul className={classes.values}>
-              {details.answer?.map((ans, index) => <li key={index+1}><p>{ans}</p></li>)}
+              {details?.answer?.map((ans, index) => <li key={index+1}><p>{ans}</p></li>)}
             </ul>
           </div>
         </div>
@@ -36,6 +36,5 @@ export default function Question({details}) {
     </div>
   )
 }
-
 
 
