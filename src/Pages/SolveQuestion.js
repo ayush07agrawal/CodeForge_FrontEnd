f4a6efa5dfc5cd3b4742c43c622c9b070333cf2c
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-06-28 02:02:11 +0530

    Update SolveQuestion.js

--- src/Pages/SolveQuestion.js
+++ src/Pages/SolveQuestion.js
@@ -2,27 +2,21 @@ import React from "react";
 import Question from "../Components/Question";
 import CodeEditor from "../Components/CodeEditor";
 import classes from "./SolveQuestion.module.css";
-import { json, useLoaderData } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useGetParticularQuestionQuery } from "../redux/api/api";
+import { useErrors } from "../hooks/hooks";
 
 export default function SolveQuestion() {
-  const data = useLoaderData();
+  const params = useParams();
+  const id = params.questionId;
+  const {data, isError, error} = useGetParticularQuestionQuery(id);
+  useErrors([{isError, error}]);
   return (
     <div className={classes.wrapper}>
       <div className={classes.QuestionNCode}>
-        <Question details={data} />
-        <CodeEditor testCase={data.testCase} />
+        <Question details={data?.question} />
+        <CodeEditor testCase={data?.question.testCase} />
       </div>
     </div>
   );
-}
-
-export async function loader({ params }) {
-  const id = params.questionId;
-  const response = await fetch("http://localhost:4173/api/v1/question/" + id);
-  if (!response.ok) {
-    throw json({ message: "Error while loading the data..." }, { status: 500 });
-  } else {
-    const resData = await response.json();
-    return resData?.question;
-  }
 }
