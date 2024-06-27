9af9404b830f8c2dd9835ee3a8faf40784ee66a6
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-06-28 01:55:02 +0530

    Update ProfileBody.jsx

--- src/Components/ProfileBody.jsx
+++ src/Components/ProfileBody.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import classes from "./ProfileBody.module.css";
-import QuestionList from "../Components/QuestionList";
-import Progressbar from "../Components/Progressbar";
+import QuestionList from "./QuestionList";
+import Progressbar from "./ProgressBar";
 
-const ProfileBody = ({questions,solved_data})=>{
-    console.log(questions);
-    console.log(solved_data);
+const ProfileBody = ({ questions, solved_data }) => {
     return(
         <div className={classes.top}>
-            <div className={classes.wrapper1}>
+            {/* <div className={classes.wrapper1}>
                 <h1>Solved Problems</h1>
-                {solved_data.map((q)=>(
-                    <QuestionList question={questions[q]} num = {q+1} key ={q} solved={true} className={classes.Ques}></QuestionList>
+                {solved_data?.map((q)=>(
+                    <QuestionList question={questions[q]} num = {q+1} key ={q._id} solved={true} className={classes.Ques}></QuestionList>
                 ))} 
-            </div>
+            </div> */}
             <div className={classes.wrapper2}>
-                <Progressbar solved={solved_data.length} total={questions.length}></Progressbar>
+                <Progressbar solved = {solved_data?.length} total = {questions?.length}></Progressbar>
             </div>
         </div>
-        
     );
 }
+
 export default ProfileBody;
