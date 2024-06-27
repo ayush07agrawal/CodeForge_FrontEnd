01e1d8dc71d7abe66b657a53eba11c3585160cd1
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-06-28 02:01:06 +0530

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,42 +2,26 @@ import React from 'react';
 import QuestionList from "../Components/QuestionList";
 import classes from "./HomePage.module.css";
 import { Link } from 'react-router-dom';
-
-const data = [
-  {
-      "Id" : "234",
-      "title" : "Reverse a String",
-      "description" : "Write a function that reverses a string.",
-      "tags" : ["easy","string"],
-      "testCase" : ["abs","bcd","aabaa","abcd","qwer"],
-      "answer" : ["sba","dcb","aabaa","dcba","rewq"]
-  },
-  {
-      "Id" : "264",
-      "title" : "ODD or Even",
-      "description" : "Write a program that take a input and tells wether input is odd or even.",
-      "tags" : ["hard"],
-      "testCase" : ["1","2","134","567","45244"],
-      "answer" : ["ODD","EVEN","EVEN","ODD","EVEN"]
-  },
-  {
-      "Id" : "235",
-      "title" : "Sum of numbers in given range",
-      "description" : "Write a function that takes two input and given the sum of numbers between them (both inclusive).",
-      "tags" : ["medium","Number Theory"],
-      "testCase" : ["1 2","1 5","10 15","1 10","1 1"],
-      "answer" : ["3","15","75","55","1"]
-  }
-]
+import { v4 as uuid } from 'uuid';
+import { useGetQuestionsQuery } from '../redux/api/api';
+import { useErrors } from '../hooks/hooks';
 
 export default function HomePage() {
+  const allQuestions = useGetQuestionsQuery();
+
+  const errors = [
+    { isError: allQuestions.isError, error: allQuestions.error },
+  ] 
+  useErrors(errors);
+
   return (    
     <div className={classes.wrapper}>
-      {data.map((q,idx)=>(
-        <Link to={"/question/"+q.Id}><QuestionList question={q} num = {idx+1} key ={idx} /></Link>
+      {allQuestions.data?.questions.map((q, idx)=>(
+        <Link to = {"question/"+q._id} key = {uuid()}>
+          <QuestionList question = {q} num = {idx+1} />
+        </Link>
       ))}      
     </div>
   )
 }
-
 
