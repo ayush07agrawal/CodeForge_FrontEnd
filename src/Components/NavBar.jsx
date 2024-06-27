44f934ad1902e979e383c3a83df6581a17c66039
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-06-28 01:54:24 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import classes from "./NavBar.module.css";
 import { NavLink } from 'react-router-dom';
 import img1 from "../Assests/Frame1.jpg";
+import { useSelector } from 'react-redux';
 
 export default function NavBar() {
+  const { user } = useSelector((state) => state.auth);
   return (
     <div className={classes.wrapper}>
-        <NavLink to="/home" className={classes.brand}><img src={img1} alt="Logo" /></NavLink>
+        <NavLink to="/" className={classes.brand}><img src={img1} alt="Logo" /></NavLink>
         <ul className={classes.links}>
           <li>
-            <NavLink to="/home" className={({isActive}) => (isActive ? classes.active : undefined)}>
+            <NavLink to="/" className={({isActive}) => (isActive ? classes.active : undefined)}>
               PRACTICE
             </NavLink>
           </li>
@@ -19,7 +21,7 @@ export default function NavBar() {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/user/:userid" className={({isActive}) => (isActive ? classes.active : undefined)}>
+            <NavLink to={`user/${user._id}`} className={({isActive}) => (isActive ? classes.active : undefined)}>
               <img src="" alt="Profile" />
             </NavLink>
           </li>
