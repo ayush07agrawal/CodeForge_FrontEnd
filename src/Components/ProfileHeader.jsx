6ea3868575c4660b92c28369605f4e1f5d734fda
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-06-28 01:55:52 +0530

    Update ProfileHeader.jsx

--- src/Components/ProfileHeader.jsx
+++ src/Components/ProfileHeader.jsx
@@ -13,32 +13,32 @@ const ProfileHeader = ({profile})=>{
     return(
         <div className={classes.wrapper}>
             <div className={classes.cover}>
-                <img src={profile[0].coverimg===""?cover:profile[0].coverimg} alt=""></img>
+                <img src={profile.coverimg===""?cover:profile.coverimg} alt=""></img>
                 <div className={classes.profilephoto}>
-                    <img src={profile[0].photo===""?pphoto:profile[0].photo} alt=""></img>
+                    <img src={profile.photo===""?pphoto:profile.photo} alt=""></img>
                 </div>
             </div>
             <div className={classes.downcover}>
-                <p className={classes.profilename}>{profile[0].name}</p>
-                <p className={classes.profileroll}>{profile[0].rollNumber}</p>
+                <p className={classes.profilename}>{profile.name}</p>
+                <p className={classes.profileroll}>{profile.rollNumber}</p>
                 <div className={classes.profilelinks}>
-                    <a href={`mailto:${profile[0].email}`} target="_blank" rel="noopener noreferrer">
-                        <img src={emailicon} alt="email" className={classes.iconlinks} style={profile[0].email===""?{display:'none'}:{}}/>
+                    <a href={`mailto:${profile.email}`} target="_blank" rel="noopener noreferrer">
+                        <img src={emailicon} alt="email" className={classes.iconlinks} style={profile.email===""?{display:'none'}:{}}/>
                     </a>
-                    <a href={profile[0].github} target="_blank" rel="noopener noreferrer">
-                        <img src={githubicon} alt="github" className={classes.iconlinks} style={profile[0].github===""?{display:'none'}:{}}/>
+                    <a href={profile.github} target="_blank" rel="noopener noreferrer">
+                        <img src={githubicon} alt="github" className={classes.iconlinks} style={profile.github===""?{display:'none'}:{}}/>
                     </a>
-                    <a href={profile[0].linkedin} target="_blank" rel="noopener noreferrer">
-                        <img src={linkednicon} alt="linkedn" className={classes.iconlinks} style={profile[0].linkedin===""?{display:'none'}:{}}/>
+                    <a href={profile.linkedin} target="_blank" rel="noopener noreferrer">
+                        <img src={linkednicon} alt="linkedn" className={classes.iconlinks} style={profile.linkedin===""?{display:'none'}:{}}/>
                     </a>
-                    <a href={profile[0].codeforces} target="_blank" rel="noopener noreferrer">
-                        <img src={codeforcesicon} alt="codeforces" className={classes.iconlinks} style={profile[0].codeforces===""?{display:'none'}:{}}/>
+                    <a href={profile.codeforces} target="_blank" rel="noopener noreferrer">
+                        <img src={codeforcesicon} alt="codeforces" className={classes.iconlinks} style={profile.codeforces===""?{display:'none'}:{}}/>
                     </a>
-                    <a href={profile[0].codechef} target="_blank" rel="noopener noreferrer">
-                        <img src={codecheficon} alt="codechef" className={classes.iconlinks} style={profile[0].codechef===""?{display:'none'}:{}}/>
+                    <a href={profile.codechef} target="_blank" rel="noopener noreferrer">
+                        <img src={codecheficon} alt="codechef" className={classes.iconlinks} style={profile.codechef===""?{display:'none'}:{}}/>
                     </a>
-                    <a href={profile[0].leetcode} target="_blank" rel="noopener noreferrer">
-                        <img src={leetcodeicon} alt="leetcode" className={classes.iconlinks} style={profile[0].leetcode===""?{display:'none'}:{}}/>
+                    <a href={profile.leetcode} target="_blank" rel="noopener noreferrer">
+                        <img src={leetcodeicon} alt="leetcode" className={classes.iconlinks} style={profile.leetcode===""?{display:'none'}:{}}/>
                     </a>                    
                 </div>
             </div>            
