d7eb008f14dbb8157fd73090836c1e712a250fd1
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-06-29 13:00:27 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import classes from "./NavBar.module.css";
 import { NavLink } from 'react-router-dom';
 import img1 from "../Assests/Frame1.jpg";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { userNotExists } from '../redux/reducers/auth';
+import axios from 'axios';
+import { server } from '../Assests/config';
+import toast from 'react-hot-toast';
 
 export default function NavBar() {
   const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const handleLogOut = async () => {
+    try {
+        const { data } = await axios.get(`${server}/api/v1/user/logOut`, { withCredentials: true });
+        dispatch(userNotExists());
+        toast.success(data.message);
+    }
+    catch(error) {
+        toast.error(error?.response?.data?.message || "Something went wrong")
+    }
+  }
+
   return (
     <div className={classes.wrapper}>
         <NavLink to="/" className={classes.brand}><img src={img1} alt="Logo" /></NavLink>
@@ -24,6 +41,9 @@ export default function NavBar() {
             <NavLink to={`user/${user._id}`} className={({isActive}) => (isActive ? classes.active : undefined)}>
               <img src="" alt="Profile" />
             </NavLink>
+            <button className = {classes.btn} onClick={handleLogOut}>
+              LogOut
+            </button>
           </li>
         </ul>    
     </div>
