d1df4979475cd654524931be16d0af989371caf3
Saurav Singh <118764502+sauravatgithub-web@users.noreply.github.com>
2024-07-08 11:53:30 +0530

    Update QuestionList.jsx

--- src/Components/QuestionList.jsx
+++ src/Components/QuestionList.jsx
@@ -8,23 +8,23 @@ import { Link } from 'react-router-dom';
 
 const QuestionList = ({ question, num, solved = false }) => {
     return (
-        <Link to = {"/app/question/"+question._id} className={classes.wrapper}>
+        <Link to = {"/app/question/"+question?._id} className={classes.wrapper}>
             <div className={classes.first}>
                 <div>
-                    <p className={classes.num}><span className={(question.tags[0] === 'easy') ? classes.easycolor :
-                        (question.tags[0] === 'medium') ? classes.mediumcolor : classes.hardcolor}>{num}</span></p>
+                    <p className={classes.num}><span className={(question?.tags[0] === 'easy') ? classes.easycolor :
+                        (question?.tags[0] === 'medium') ? classes.mediumcolor : classes.hardcolor}>{num}</span></p>
                 </div>
                 <div>
-                    <p className={classes.title}>{question.title}</p>
+                    <p className={classes.title}>{question?.title}</p>
                     <div className={classes.tags}>
-                        {question.tags.map((tag, idx) => (
+                        {question?.tags.map((tag, idx) => (
                             <span key={idx} className={classes.tag}>{tag}</span>
                         ))}
                     </div>
                 </div>
             </div>
             <div className={classes.second}>
-                <img src={(solved === true) ? solvedimg : (question.tags[0] === 'easy') ? easyimg : (question.tags[0] === 'medium') ? mediumimg : hardimg} alt="" />
+                <img src={(solved === true) ? solvedimg : (question?.tags[0] === 'easy') ? easyimg : (question?.tags[0] === 'medium') ? mediumimg : hardimg} alt="" />
             </div>
         </Link>
     );
